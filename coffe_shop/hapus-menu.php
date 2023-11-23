72bab5189d46c4fb219dea138af27baa42e83b94
Nisa97596 <111674074+Nisa97596@users.noreply.github.com>
2023-11-23 20:08:51 +0700

    Delete coffe_shop directory

--- coffe_shop/hapus-menu.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$id_menu = $_GET['id'];
-mysqli_query($koneksi, "delete from menu where id_menu='$id_menu'");
-
-header("location: menu.php");
-?>
--- coffe_shop/hapus-transaksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$id_transaksi = $_GET['id'];
-mysqli_query($koneksi, "delete from transaksi where id_transaksi='$id_transaksi'");
-
-header("location: transaksi.php");
-?>
